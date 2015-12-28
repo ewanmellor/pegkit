0a17ccfc214ea2ded03ead186d979f6a5f32f877
Ewan Mellor <commits@ewanmellor.org>
2015-12-27 23:10:41 -0800

    Move offset declaration to a header file.

---
+++ include/PEGKit/PKToken+Subclass.h
@@ -0,0 +1,15 @@
+//
+//  PKToken+Subclass.h
+//  PEGKit
+//
+//  Created by Ewan Mellor on 12/27/15.
+//
+//
+
+#import <PEGKit/PKToken.h>
+
+@interface PKToken ()
+
+@property (nonatomic, readwrite) NSUInteger offset;
+
+@end
